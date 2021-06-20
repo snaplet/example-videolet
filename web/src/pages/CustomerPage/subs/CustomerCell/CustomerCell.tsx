a89fc075c38a7aec04c8214e63f36adac22882bb
Peter Pistorius <peter.pistorius@gmail.com>
2021-06-20 17:25:16 +0200

    Add bug.

--- web/src/pages/CustomerPage/subs/CustomerCell/CustomerCell.tsx
+++ web/src/pages/CustomerPage/subs/CustomerCell/CustomerCell.tsx
@@ -41,18 +41,55 @@ export const Success = ({ customer }: Customer) => {
 
       {customer.email}
 
-      <h3>Rentals</h3>
+      <h3>Rentals:</h3>
       <ul>
         {customer.rentals.map((r) => {
+          const d1 = new Date(r.rentalDate)
+          const d2 = new Date(r.returnDate) // when `returnDate` is null it defaults to "epoch" time.
+
+          let isReturned = false
+          if (typeof r.returnDate === 'string') {
+            isReturned = true
+          }
+
+          const dayInHours = 86_400_000
+          const totalDays = Math.round(Math.abs((d2 - d1) / dayInHours))
+
+          const rentalStart = f.format(d1)
+          // Format return date. If return date is the same as epoch time then it is not returned.
+          const rentalEnd =
+            d2.getTime() === new Date(0).getTime()
+              ? f.format(new Date()) + ' (not returned)'
+              : f.format(d2)
+
           return (
             <li key={r.id}>
+              <b>Status:</b>{' '}
+              <pre style={{ display: 'inline' }}>
+                {!isReturned ? (
+                  <span style={{ color: 'red' }}>Not returned</span>
+                ) : (
+                  <span style={{ color: 'green' }}>Returned</span>
+                )}
+              </pre>
+              <br />
+              <b>Rental period:</b>{' '}
+              <pre style={{ display: 'inline' }}>{totalDays} days</pre>
+              <br />
+              <b>Amount:</b>{' '}
+              <pre style={{ display: 'inline' }}>
+                <span style={{ color: isReturned ? '' : 'red' }}>
+                  ${(r.film.cost * totalDays).toFixed(2)}
+                </span>
+              </pre>
+              <br />
+              <b>Period:</b>{' '}
+              <pre style={{ display: 'inline' }}>
+                {rentalStart} - {rentalEnd}
+              </pre>
+              <br />
               {r.film.title}
               <br />
-              <pre>
-                {f.format(new Date(r.rentalDate))} -{' '}
-                {f.format(new Date(r.returnDate))}
-              </pre>
-
               <br />
             </li>
           )
