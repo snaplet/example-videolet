0a58e6458722179944b5087f93f1a1b3d4ca576d
Peter Pistorius <peter.pistorius@gmail.com>
2021-06-23 11:45:41 +0200

    Use transaction for customers.

--- api/db/seed.js
+++ api/db/seed.js
@@ -16,27 +16,45 @@ const db = new PrismaClient()
 async function main() {
   // Customers
   const films = [
-    { title: 'prisma', description: 'epic drama of prisma', release_year: '2007', language_id: 1, rental_duration: 3, rental_rate: 0.99, length: 90, rating: 'PG' },
+    {
+      title: 'prisma',
+      description: 'epic drama of prisma',
+      release_year: '2007',
+      language_id: 1,
+      rental_duration: 3,
+      rental_rate: 0.99,
+      length: 90,
+      rating: 'PG',
+    },
   ]
 
   const customers = [
-    { first_name: 'tony', last_name: 'stark', email: 'tony@example.com' },
-    { first_name: 'mark', last_name: 'stark', email: 'mark@example.com' },
-    { first_name: 'jackie', last_name: 'stark', email: 'jackie@example.com' },
-    { first_name: 'bob', last_name: 'stark', email: 'bob@example.com' },
+    { firstName: 'tony', lastName: 'stark', email: 'tony@example.com' },
+    { firstName: 'mark', lastName: 'stark', email: 'mark@example.com' },
+    { firstName: 'jackie', lastName: 'stark', email: 'jackie@example.com' },
+    { firstName: 'bob', lastName: 'stark', email: 'bob@example.com' },
   ]
 
-  console.log("Starting")
+  console.log('Starting')
 
-  console.log(db);
-  console.log(db.customer);
+  //console.log(db)
+  //console.log(db.customer)
 
-  await customers.map(user => {
-    const record = db.customer.create({
-      data: user
+  //)
+
+  // const x = await db.film.create({
+  //   data: films[0],
+  // })
+  // console.log(x)
+
+  const inserts = customers.map((user) => {
+    return db.customer.create({
+      data: user,
     })
-    console.log(record)
   })
+  const y = await db.$transaction(inserts)
+
+  console.log(y)
 }
 
 main()
