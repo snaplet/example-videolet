72f5a9a92c7e0f2bf6721a0050ca0baba222e5fa
Peter Pistorius <peter.pistorius@gmail.com>
2021-06-23 12:33:50 +0200

    Update seed file.

--- api/db/seed.js
+++ api/db/seed.js
@@ -14,47 +14,97 @@ const db = new PrismaClient()
  * @see https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#createmany
  */
 async function main() {
-  // Customers
-  const films = [
-    {
-      title: 'prisma',
-      description: 'epic drama of prisma',
-      release_year: '2007',
-      language_id: 1,
-      rental_duration: 3,
-      rental_rate: 0.99,
-      length: 90,
-      rating: 'PG',
-    },
-  ]
+  let address = await db.address.findFirst({ where: { address_id: 1 } })
+  if (!address) {
+    // Create an address...
+    address = await db.address.create({
+      data: {
+        address: 'Steak and chips 55',
+        district: 'X',
+        phone: '555-1234',
+        city: {
+          connectOrCreate: {
+            where: {
+              city_id: 1,
+            },
+            create: {
+              city: 'Loncastershire',
+              country: {
+                connectOrCreate: {
+                  where: {
+                    country_id: 1,
+                  },
+                  create: {
+                    country: 'England',
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    })
+  }
+
+  let staff = await db.staff.findUnique({ where: { staff_id: 1 } })
+  if (!staff) {
+    staff = await db.staff.create({
+      data: {
+        first_name: 'Steve',
+        last_name: 'Stevenson',
+        username: 'steve.stevenson',
+        password: 'hunter2',
+        address: {
+          connect: {
+            address_id: address.address_id,
+          },
+        },
+        store_id: 1,
+      },
+    })
+  }
+
+  let store = await db.store.findUnique({ where: { store_id: 1 } })
+  if (!store) {
+    store = await db.store.create({
+      data: {
+        address: {
+          connect: {
+            address_id: address.address_id,
+          },
+        },
+        manager_staff_id: 1,
+      },
+    })
+  }
+
+  console.log('Starting...')
 
   const customers = [
-    { firstName: 'tony', lastName: 'stark', email: 'tony@example.com' },
     { firstName: 'mark', lastName: 'stark', email: 'mark@example.com' },
     { firstName: 'jackie', lastName: 'stark', email: 'jackie@example.com' },
     { firstName: 'bob', lastName: 'stark', email: 'bob@example.com' },
   ]
-
-  console.log('Starting')
-
-  //console.log(db)
-  //console.log(db.customer)
-
-  //)
-
-  // const x = await db.film.create({
-  //   data: films[0],
-  // })
-  // console.log(x)
-
   const inserts = customers.map((user) => {
     return db.customer.create({
-      data: user,
+      data: {
+        ...user,
+        address: {
+          connect: {
+            address_id: address.address_id,
+          },
+        },
+        store: {
+          connect: {
+            store_id: store.store_id,
+          },
+        },
+      },
     })
   })
   const y = await db.$transaction(inserts)
 
-  console.log(y)
+  console.log('... Done')
 }
 
 main()
--- web/src/pages/FilmsPage/subs/FilmsCell/FilmsCell.tsx
+++ web/src/pages/FilmsPage/subs/FilmsCell/FilmsCell.tsx
@@ -18,18 +18,21 @@ export const Loading = () => {
 
 export const Success = ({ films }: Films) => {
   return (
-    <ol>
-      {films.map((f) => {
-        return (
-          <li key={f.id}>
-            <b>{f.title}</b> - ({f.year}) - <b>${f.cost}</b>
-            <br />
-            {f.description}
-            <br />
-            <br />
-          </li>
-        )
-      })}
-    </ol>
+    <>
+      {!films.length && 'No films available'}
+      <ol>
+        {films.map((f) => {
+          return (
+            <li key={f.id}>
+              <b>{f.title}</b> - ({f.year}) - <b>${f.cost}</b>
+              <br />
+              {f.description}
+              <br />
+              <br />
+            </li>
+          )
+        })}
+      </ol>
+    </>
   )
 }
